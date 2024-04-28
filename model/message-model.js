95f23966b3227fbcd4a64d29b380cc5788136ac2
fw21_0822 <mdmonu2021@gmail.com>
2024-04-28 15:24:59 +0530

    date format changes

--- model/message-model.js
+++ model/message-model.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
 
+function ChangeDateFormat() {
+  let date_string = new Date();
+  let day = date_string.getDate();
+  let month = date_string.getMonth() + 1;
+  let year = date_string.getFullYear();
+  let hours = date_string.getHours();
+  let minutes = date_string.getMinutes();
+  console.log(day + "/" + month + "/" + year + " " + hours + ":" + minutes);
+
+  let new_format = `${day}/${month}/${year} ${hours}:${minutes}`;
+  console.log("new date", new_format);
+  return new_format;
+}
+
 const messageSchema = new mongoose.Schema({
-  date: { type: Date, default: new Date() },
+  date: { type: String, default: ChangeDateFormat() },
   room_id: { type: String, required: true },
   message: { type: String, required: true },
   user_email: { type: String, required: true },
